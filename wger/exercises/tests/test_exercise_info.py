df781314cf17a58e23c2c66faaa93bd471117ed7
lederp23 <munalaoliverngoitsi@gmail.com>
2017-03-14 18:30:30 +0300

    Tested api response content

--- wger/exercises/tests/test_exercise_info.py
+++ wger/exercises/tests/test_exercise_info.py
@@ -11,4 +11,21 @@ class TestShowExerciseInfo(WorkoutManagerTestCase):
         """Tests for showing all info exercises"""
         response = self.client.get('/api/v2/exerciseinfo/')
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(len(response.data) > 0)
+        self.assertTrue(response.data['count'])
+        self.assertTrue(str(response.data['results'][0]['status']) is not None)
+        self.assertTrue(str(response.data['results'][
+                        0]['description']) is not None)
+        self.assertTrue(str(response.data['results'][0]['name']) is not None)
+        self.assertTrue(str(response.data['results'][0][
+                        'creation_date']) is not None)
+        self.assertTrue(str(response.data['results'][0]['uuid']) is not None)
+        self.assertTrue(str(response.data['results'][0]['license']) is not None)
+        self.assertTrue(str(response.data['results'][
+                        0]['category']) is not None)
+        self.assertTrue(str(response.data['results'][
+                        0]['language']) is not None)
+        self.assertTrue(str(response.data['results'][0]['muscles']) is not None)
+        self.assertTrue(str(response.data['results'][0][
+                        'muscles_secondary']) is not None)
+        self.assertTrue(str(response.data['results'][
+                        0]['equipment']) is not None)
